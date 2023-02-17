6c7e90a7a51f4aa296a3ef65a65e3cd02cd81b88
Nikita Rossenko <darkcaty1999@live.ru>
2023-02-17 16:49:59 +0200

    Added login/register Pages

---
+++ src/login.ts
@@ -0,0 +1,40 @@
+const allUsersString = localStorage.getItem('allUsers');
+
+if (!allUsersString){
+    const emptyArray:Array<User> = [];
+    localStorage.setItem('allUsers',JSON.stringify(emptyArray))
+}
+
+function loginHandler(e){
+    e.preventDefault();
+    const allUsersString = localStorage.getItem('allUsers');
+    const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
+    const passwordInput = document.querySelector("#password");
+    const popMessage = document.querySelector('#loginPopMessage');
+    localStorage.setItem('isLoggedIn','false');
+
+    if(allUsersString){
+        const allUsersArray = JSON.parse(allUsersString);
+        let foundUser = false;
+        if (username && password){
+            allUsersArray.forEach(user => {
+                if (user.username.toLowerCase() === username.toLowerCase() && user.password === password){
+                    console.log("okay")
+                    foundUser = true;
+                    const isLoggedInString = `{"isLoggedIn":true,"username":"${user.username}"}`
+                    localStorage.setItem('isLoggedIn',isLoggedInString)
+                    location.href = 'index.html'
+                }
+            });
+            if (!foundUser){
+                if (passwordInput && !popMessage) {
+                    passwordInput.insertAdjacentHTML(
+                        "afterend",
+                        "<span id='loginPopMessage'>Username don't exists or wrong password!</span>"
+                    );
+                }
+            }
+        }
+    }
+}
